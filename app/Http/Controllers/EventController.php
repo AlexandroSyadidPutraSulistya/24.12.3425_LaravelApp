3bce155c52197a0225c62ef45395e9a1b83686c0
AlexandroSyadidPutraSulistya <alexandrosyadid2006@students.amikom.ac.id>
2026-06-10 14:29:35 +0700

    Menambahkan Alur Upload Event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
-    public function show(){
-        return view('event-detail');
+    public function show($id){
+    // Mengambil daftar kategori untuk keperluan menu footer
+    // $categories = \App\Models\Category::all();
+    $event = Event::with('category')->findOrFail($id);
+    
+        // Me-render view dengan membawa data kategori dan data spesifik acara tersebut
+        return view('event-detail', compact('event'));
     }
 
     public function checkout(){
